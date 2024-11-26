5a38ee1fb3cd19e0726b529da873b5980fae55c3
“Ale <alecuevas@134340.local>
2024-11-26 06:33:37 -0600

    se agregó el hola mundo

---
+++ hola.cpp
@@ -0,0 +1,7 @@
+#include <iostream>                                                //directiva para manejar entrada/salida
+using namespace std;                                               //espacio de nombres para evitar confusiones o dejar de escribir std
+
+int main(){                                                        //función principal
+  cout << "Hola desde c++, estoy corriendo en colab!" << endl;     //cout es salida, endl es salto de línea
+  return 0;                                                        //el programa ha terminado correctamente, el programa termina sin errores
+}
